db9628e48ff57daf4b20663c7f7f4b581db05e0d
hstrohm <54555630+hstrohm@users.noreply.github.com>
2021-04-28 14:05:01 -0500

    Update filter_group.py
    comment out unused groups

--- tf_coder/filter_group.py
+++ tf_coder/filter_group.py
@@ -84,40 +84,40 @@ class FilterGroup(enum.Enum):
   SAME_DTYPE_NUMERIC_BROADCASTABLE_2 = 'SAME_DTYPE_NUMERIC_BROADCASTABLE_2'
   # The two arguments are float tensors with the same dtype and the two tensors
   # are broadcastable, or one of the arguments is a float primitive.
-  SAME_DTYPE_FLOAT_BROADCASTABLE_2 = 'SAME_DTYPE_FLOAT_BROADCASTABLE_2'
+  #SAME_DTYPE_FLOAT_BROADCASTABLE_2 = 'SAME_DTYPE_FLOAT_BROADCASTABLE_2'
   # Of the two arguments, one must be a SparseTensor, and the other must be a
   # Tensor or SparseTensor. The two arguments must have the same shape.
-  SAME_SHAPE_ONE_SPARSE_2 = 'SAME_SHAPE_ONE_SPARSE_2'
+  #SAME_SHAPE_ONE_SPARSE_2 = 'SAME_SHAPE_ONE_SPARSE_2'
   # The two arguments arguments must be SparseTensors with the same shape.
-  SAME_SHAPE_BOTH_SPARSE_2 = 'SAME_SHAPE_BOTH_SPARSE_2'
+  #SAME_SHAPE_BOTH_SPARSE_2 = 'SAME_SHAPE_BOTH_SPARSE_2'
   # The first argument is an int representing an axis, the second is a list of
   # SparseTensor.
-  AXIS_SPARSESEQUENCE_2 = 'AXIS_SPARSESEQUENCE_2'
+  #AXIS_SPARSESEQUENCE_2 = 'AXIS_SPARSESEQUENCE_2'
   # The first argument is a SparseTensor, the second is an axis.
-  SPARSE_AXIS_2 = 'SPARSE_AXIS_2'
+  #SPARSE_AXIS_2 = 'SPARSE_AXIS_2'
   # The first argument is a SparseTensor, the second is a shape.
-  SPARSE_SHAPE_2 = 'SPARSE_SHAPE_2'
+  #SPARSE_SHAPE_2 = 'SPARSE_SHAPE_2'
   # The first argument is a SparseTensor, the second is a primitive.
-  SPARSE_PRIMITIVE_2 = 'SPARSE_PRIMITIVE_2'
+  #SPARSE_PRIMITIVE_2 = 'SPARSE_PRIMITIVE_2'
   # The first argument is an int or float tensor, and the second argument is a
   # 1D int tensor with nonnegative nondecreasing entries, and the two arguments
   # have equal lengths in the first dimension.
-  SEGMENT_OPERATION_2 = 'SEGMENT_OPERATION_2'
+  #SEGMENT_OPERATION_2 = 'SEGMENT_OPERATION_2'
 
   ##############################
   # Operations with 3 arguments.
 
   # The first argument is a SparseTensor, the second is an int, and the third is
   # an axis.
-  SPARSE_INT_AXIS_3 = 'SPARSE_INT_AXIS_3'
+  #SPARSE_INT_AXIS_3 = 'SPARSE_INT_AXIS_3'
   # The first argument is a SparseTensor, the second is an axis, the third is a
   # boolean.
-  SPARSE_AXIS_BOOL_3 = 'SPARSE_AXIS_BOOL_3'
+  #SPARSE_AXIS_BOOL_3 = 'SPARSE_AXIS_BOOL_3'
   # The first argument is an int or float tensor, the second argument is an int
   # tensor with nonnegative entries and with a shape that is a prefix of the
   # first argument's shape, and the third argument is an integer greater than
   # the maximum element of the second argument.
-  UNSORTED_SEGMENT_OPERATION_3 = 'UNSORTED_SEGMENT_OPERATION_3'
+  #UNSORTED_SEGMENT_OPERATION_3 = 'UNSORTED_SEGMENT_OPERATION_3'
 
   #########################################
   # Operations with other special handling.
@@ -129,7 +129,7 @@ class FilterGroup(enum.Enum):
   # The argument results in a small tensor.
   RANGE_1 = 'RANGE_1'
   # The argument contains a few ints with a small maximum.
-  SEQUENCE_MASK_1 = 'SEQUENCE_MASK_1'
+  #SEQUENCE_MASK_1 = 'SEQUENCE_MASK_1'
   # The argument is a tensor with at least one squeezable dimension.
   SQUEEZE_1 = 'SQUEEZE_1'
 
@@ -148,7 +148,7 @@ class FilterGroup(enum.Enum):
   GATHER_2 = 'GATHER_2'
   # The first argument is a tensor, the second is a tensor containing ints
   # suitable for indexing into the first tensor on multiple dimensions.
-  GATHER_ND_2 = 'GATHER_ND_2'
+  #GATHER_ND_2 = 'GATHER_ND_2'
   # Ensures the tensors are both numeric and have the same dtype and rank.
   MATMUL_2 = 'MATMUL_2'
   # Ensures that tf.one_hot(indices, depth) produces a small result.
@@ -162,25 +162,22 @@ class FilterGroup(enum.Enum):
   # primitive or tensor.
   TOP_K_2 = 'TOP_K_2'
   # The first argument is a tensor, the second is 1D (ints) with correct length.
-  TRANSPOSE_2 = 'TRANSPOSE_2'
+  #TRANSPOSE_2 = 'TRANSPOSE_2'
   # The first argument is sorted in the last dimension, the second argument is
   # the same dtype and rank, and all dimension lengths match except the last.
   SEARCHSORTED_2 = 'SEARCHSORTED_2'
   # The first argument contains a few ints with a small maximum, and the second
   # is an int primitive or tensor with a value in range.
-  SEQUENCE_MASK_2 = 'SEQUENCE_MASK_2'
+  #SEQUENCE_MASK_2 = 'SEQUENCE_MASK_2'
   # Ensures that tf.sparse.retain(sp_input, to_retain) has arguments that match,
   # i.e., to_retain is 1D, contains booleans, and has the right length.
-  SPARSE_RETAIN_2 = 'SPARSE_RETAIN_2'
-  # Ensures that tf.sparse.to_indicator(sp_input, vocab_size) produces a small
-  # result.
-  SPARSE_TO_INDICATOR_2 = 'SPARSE_TO_INDICATOR_2'
+  #SPARSE_TO_INDICATOR_2 = 'SPARSE_TO_INDICATOR_2'
   # The first argument is a SparseTensor, the second is 1D (ints) with correct
   # length.
-  SPARSE_TRANSPOSE_2 = 'SPARSE_TRANSPOSE_2'
+  #SPARSE_TRANSPOSE_2 = 'SPARSE_TRANSPOSE_2'
   # The first argument is a tensor with more than 1 squeezable dimension, and
   # the second argument is an int specifying a squeezable dimension.
-  SQUEEZE_2 = 'SQUEEZE_2'
+  #SQUEEZE_2 = 'SQUEEZE_2'
 
   # The first argument must be an int or float tensor, and the second and third
   # arguments must both be nondecreasing primitives.
@@ -188,7 +185,7 @@ class FilterGroup(enum.Enum):
   # The first argument is a tensor, the second is a tensor containing ints
   # suitable for indexing into the first tensor on multiple dimensions, and the
   # third is a number of batch dimensions.
-  GATHER_ND_3 = 'GATHER_ND_3'
+  #GATHER_ND_3 = 'GATHER_ND_3'
   # The first argument must be a tensor, the second must be a nested int list or
   # int32 tensor of shape [rank_of_arg_1, 2], and the third must be a scalar of
   # the same type as the first argument.
@@ -201,16 +198,16 @@ class FilterGroup(enum.Enum):
   # In tf.scatter_nd(indices, updates, shape), `indices` must be an int tensor
   # representing valid indices, `shape` must be a shape tuple, and `updates`
   # must have a compatible shape with `indices` and `shape`.
-  SCATTER_ND_3 = 'SCATTER_ND_3'
+  #SCATTER_ND_3 = 'SCATTER_ND_3'
   # In tf.sparse.slice(sp_input, start, size), the arguments start and size must
   # be 1D (ints) with the same length as sp_input.dense_shape.
-  SPARSE_SLICE_3 = 'SPARSE_SLICE_3'
+  #SPARSE_SLICE_3 = 'SPARSE_SLICE_3'
   # The arguments must have the right shape to make a SparseTensor.
-  SPARSETENSOR_3 = 'SPARSETENSOR_3'
+  #SPARSETENSOR_3 = 'SPARSETENSOR_3'
   # In tf.tensor_scatter_nd_update(tensor, indices, updates), `indices` must be
   # an int tensor representing valid indices, and `updates` must have a
   # compatible shape and be the same dtype as `tensor`.
-  TENSOR_SCATTER_ND_UPDATE_3 = 'TENSOR_SCATTER_ND_UPDATE_3'
+  #TENSOR_SCATTER_ND_UPDATE_3 = 'TENSOR_SCATTER_ND_UPDATE_3'
   # The first two arguments are tensors with the same dtype, and the third
   # contains ints of the appropriate shape.
   TENSORDOT_3 = 'TENSORDOT_3'
@@ -219,6 +216,6 @@ class FilterGroup(enum.Enum):
 
   # Ensures that params, indices, axis, and batch_dims are all compatible
   # according to the tf.gather documentation.
-  GATHER_4 = 'GATHER_4'
+  #GATHER_4 = 'GATHER_4'
 
 # LINT.ThenChange(value_search/operation_filtering.py:add_filters_to_function_operation)
